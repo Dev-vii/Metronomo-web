432e27e0e0fb28fadd521a522199134e7943cbcb
Vicente <carvalho.vicente0207@gmail.com>
2025-01-30 13:56:30 -0300

    Add botão play/pause + inicio do sistema de vel

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let play_sumir = document.querySelector(".play")
+let pause_aparecer = document.querySelector(".pause")
 function mudapause(){
     play_sumir.style.display = "none"
+    pause_aparecer.style.display = "block"
+}
+function mudaplay(){
+    play_sumir.style.display = "block"
+    pause_aparecer.style.display = "none"
 }
