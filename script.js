f0e66b8212a8e75836660e3c97d08233f4c780aa
Vicente <carvalho.vicente0207@gmail.com>
2025-02-05 18:44:54 -0300

    Add movimento da haste + sistema base de velocidade

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let play_sumir = document.querySelector(".play")
 let pause_aparecer = document.querySelector(".pause")
+let hastemexer = document.querySelector(".haste")
 function mudapause(){
     play_sumir.style.display = "none"
     pause_aparecer.style.display = "block"
+    hastemexer.style.rotate = "50deg"
+    hastemexer.style.transition = "3s"
 }
 function mudaplay(){
     play_sumir.style.display = "block"
     pause_aparecer.style.display = "none"
+    hastemexer.style.rotate = "0deg"
+    hastemexer.style.transition = "0.5s"
 }
