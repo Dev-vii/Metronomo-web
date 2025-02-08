61fbb794a6fbb5ef204803ce16bdcd710c18c71a
Vicente <carvalho.vicente0207@gmail.com>
2025-02-08 14:57:28 -0300

    Botão de velocidade melhorado

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let play_sumir = document.querySelector(".play")
 let pause_aparecer = document.querySelector(".pause")
 let hastemexer = document.querySelector(".haste")
-function mudapause(){
+
+// PAUSE
+function mudapause() { 
     play_sumir.style.display = "none"
     pause_aparecer.style.display = "block"
     hastemexer.style.rotate = "50deg"
     hastemexer.style.transition = "1s"
 }
-function mudaplay(){
+// PLAY
+function mudaplay() {
     play_sumir.style.display = "block"
     pause_aparecer.style.display = "none"
     hastemexer.style.rotate = "0deg"
     hastemexer.style.transition = "0s"
+}
+
+// MUDAR VELOCIDADE
+let velmedia = 60;
+function aumentavel() {
+    velmedia++;
+    document.getElementById("velmedia").innerText = velmedia;
+}
+
+function diminuivel() {
+    velmedia--;
+    document.getElementById("velmedia").innerText = velmedia;
 }
