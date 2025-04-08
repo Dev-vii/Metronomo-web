9b8323a1b3654cfc593da730da17439e65e1203d
Vicente <carvalho.vicente0207@gmail.com>
2025-04-08 20:25:32 -0300

    Projeto reiniciado

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
 let play_sumir = document.querySelector(".play")
 let pause_aparecer = document.querySelector(".pause")
-let hastemexer = document.querySelector(".haste")
 
 // PAUSE
 function mudapause() { 
     play_sumir.style.display = "none"
     pause_aparecer.style.display = "block"
-    hastemexer.style.rotate = "50deg"
-    hastemexer.style.transition = "1s"
 }
 // PLAY
 function mudaplay() {
     play_sumir.style.display = "block"
     pause_aparecer.style.display = "none"
-    hastemexer.style.rotate = "0deg"
-    hastemexer.style.transition = "0s"
-}
-
-// MUDAR VELOCIDADE
-let velmedia = 60;
-function aumentavel() {
-    velmedia++;
-    document.getElementById("velmedia").innerText = velmedia;
-}
-
-function diminuivel() {
-    velmedia--;
-    document.getElementById("velmedia").innerText = velmedia;
 }
