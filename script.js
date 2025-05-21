884f5c429792fcecb3f8d3a2fdfa55da5cf263db
Vicente <carvalho.vicente0207@gmail.com>
2025-05-21 14:56:16 -0300

    Som implementado

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let play_sumir = document.querySelector(".play")
 let pause_aparecer = document.querySelector(".pause")
+const click = document.getElementById('click')
 
 // PAUSE
-function mudapause() { 
+function mudapause() {
     play_sumir.style.display = "none"
     pause_aparecer.style.display = "block"
+    return true
 }
+
+do {
+    click.play()
+} while (mudapause = true)
+
 // PLAY
 function mudaplay() {
     play_sumir.style.display = "block"
